087a69a0f01705d8d16fd5c7299462ccd29203ee
Mike Selmani <mike_selmani@hotmail.com>
2023-04-09 11:59:44 -0400

    configure validation constraints for email input with styling and error messages

--- formvalidation.js
+++ formvalidation.js
@@ -1,9 +1,30 @@
-const email = document.getElementById("mail");
+const form = document.querySelector("form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
 
 email.addEventListener("input", () => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
-  } else {
-    email.setCustomValidity("");
-  }
+    if (email.validity.valid) {
+        emailError.textContent = "";
+        emailError.className = "error";
+    } else {
+        showError();
+    }
 });
+
+form.addEventListener("submit", (event) => {
+    if (!email.validity.valid) {
+        showError();
+        event.preventDefault();
+    }
+})
+
+function showError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = "This field is required.";
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = "This value needs to be an email address.";
+    } else if (email.validity.tooShort) {
+        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    }
+    emailError.className = "error active";
+};
